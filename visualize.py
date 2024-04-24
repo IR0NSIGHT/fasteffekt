97c2b73cf4100869955891f54dfa3173fd337351
IR0NSIGHT <irnsght@googlemail.com>
2024-04-24 18:34:52 +0200

    commit before investigating normaizlation error

--- visualize.py
+++ visualize.py
@@ -75,8 +75,9 @@ def extract_mean_values(statistics_table, backend):
         
         # Extract values from the "Arithmetic Mean" column
         mean_values = filtered_df['Arithmetic Mean'].tolist()
-        
-        return mean_values
+        result_dict = filtered_df.set_index('Benchmark')['Arithmetic Mean'].to_dict()
+
+        return result_dict
     except Exception as e:
         print(f"Error occurred while extracting mean values: {e}")
         return None
@@ -128,10 +129,85 @@ def normalize_data_to_awfy():
     
     # Normalize the performance values of all backends to 'awfy-javascript'
     for backend, values in data.items():
-        normalized_values = [np.round(value / awfy_values[i]) for i, value in enumerate(values)]
-        normalized_data[backend] = normalized_values
-    
+        newvalues = {}
+        for k,v in values.items():
+            newvalues[k] = v/awfy_values[k]
+        normalized_data[backend] = newvalues
     return normalized_data
+
+def plot_mean_performance_by_benchmark(df):
+    # Get unique benchmarks and their positions
+    benchmarks = df['Benchmark'].unique()
+    benchmark_positions = {benchmark: i for i, benchmark in enumerate(benchmarks)}
+
+    unique_backends = df['Backend'].unique()
+    # Set up the figure
+    plt.figure(figsize=(10, 6))
+
+    # Plot each backend's data points with the same color
+    for backend in unique_backends:
+        backend_data = df[df['Backend'] == backend]
+        x_values = [benchmark_positions[benchmark] for benchmark in backend_data['Benchmark']]
+        y_values = backend_data['Relative']
+        plt.scatter(x_values, y_values, label=backend)
+
+    # Set x-axis labels and tick positions
+    plt.xticks(range(len(benchmarks)), benchmarks, rotation=30)
+    plt.xlabel('Benchmark')
+    plt.subplots_adjust(right=0.75)  # Adjust the right margin
+    # Set y-axis label
+    plt.ylabel('Relative durchschnittliche Performanz')
+    plt.ylim(min(df['Relative']) - 0.1, max(df['Relative']) + 0.1)
+    # Add legend
+    plt.legend(title='Backend', bbox_to_anchor=(1.05, 1), loc='upper left')
+
+    # Add grid
+    plt.grid(True)
+
+    # Set title
+    plt.title("Normalisierte Performanz pro Benchmark und Backend")
+
+  
+    plt.savefig("fasteffekt_scatter_normalized.png")
+
+def analyze_overall():
+    normalized_benchmark_means_by_backend = normalize_data_to_awfy()
+    listof_datapoints = [list(pointdict.values()) for pointdict in list(normalized_benchmark_means_by_backend.values())]
+    generate_multiple_boxplots(
+        listof_datapoints,
+        normalized_benchmark_means_by_backend.keys(),
+        "Normalized mean performance",
+        "Relative to awfy-javascript")
+
+    cumulative_perf_by_benchmark_backend = pd.DataFrame(columns=['Backend', 'Benchmark', 'Relative'])
+    i = 0
+    for benchmark in benchmark_names:
+        for backend, means in normalized_benchmark_means_by_backend.items():
+            benchmark_mean = -1
+            if benchmark in means:
+                benchmark_mean = means[benchmark]
+            cumulative_perf_by_benchmark_backend = cumulative_perf_by_benchmark_backend._append({
+                "Backend": backend, "Benchmark": benchmark, 'Relative': benchmark_mean
+            }, ignore_index=True)
+        i = i + 1
+    cumulative_perf_by_benchmark_backend.to_csv(f"fasteffekt_mean_performance_by_benchmark.csv", index=False)
+    plot_mean_performance_by_benchmark(cumulative_perf_by_benchmark_backend)
+
+    plt.savefig(f"fasteffekt_mean_performance_normalized.png")
+
+
+    normalized_statistics_KPI = pd.DataFrame(columns=['Backend', 'Minimum', 'Maximum', 'Median', "Standard Deviation", "Coefficient of Variation" ,'Arithmetic Mean',])
+
+
+    for backend, means in normalized_benchmark_means_by_backend.items():
+        minimum, maximum, median, std_dev, mean, cv = calculate_statistics(list(means.values()))
+        normalized_statistics_KPI = normalized_statistics_KPI._append({'Backend': backend,'Minimum': minimum,
+                                                    'Maximum': maximum, 'Arithmetic Mean': mean, "Standard Deviation": std_dev,
+                                                    'Median': median, "Coefficient of Variation": cv}, ignore_index=True)
+
+    normalized_statistics_KPI.to_csv(f"fasteffekt_mean_performance_KPI.csv", index=False)
+
+
 
 js_effekt = read_json_file("./fasteffekt500_js.json")
 monadic = read_json_file("./fasteffekt500_monadic.json")
@@ -158,16 +234,8 @@ benchmark_names = ["permute", "nbody", "list", "mandelbrot", "bounce", "towers",
 for i in benchmark_names:
     draw_save_boxplot(i)
 
-statistics_table.to_csv(f"fasteffekt_keyvalues.csv", index=False)
+statistics_table.to_csv(f"fasteffekt_benchmarks_KPI.csv", index=False)
 
-normalized_benchmark_means_by_backend = normalize_data_to_awfy()
-generate_multiple_boxplots(
-    normalized_benchmark_means_by_backend.values(),
-    normalized_benchmark_means_by_backend.keys(),
-    "Normalized mean performance",
-    "Relative to awfy-javascript")
-
-filename = f"fasteffekt_mean_performance_normalized.png"
-plt.savefig(filename)
+analyze_overall()
 
 
